331ca6ac7953a2c9d9ac30f49029166de78da59c
yoshisaac <isfortner@gmail.com>
2024-05-12 21:51:55 -0400

    host info & logo arg

--- fetch.py
+++ fetch.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 import sys, os, subprocess
-"""
-i = 0
-while i < len(sys.argv):
-    if sys.argv[i]
-    i = i + 1
-"""
-osf = open('os.txt', 'r').read().split("\n")
-oslogo = [str] * len(osf)
 
 uname = subprocess.check_output("uname -a", shell=True, text=True).split(" ")
+whoami = subprocess.check_output("whoami", shell=True, text=True).strip()
 cpuinfo = subprocess.check_output("cat /proc/cpuinfo", shell=True, text=True).split("\n")
 osinfo = subprocess.check_output("cat /etc/os-release", shell=True, text=True).split("\n")
 
+i = 1
+while i < len(sys.argv):
+    if not sys.argv[i] == "-d" and not sys.argv[i] == "--distro":
+        osinfo[2] = sys.argv[i]
+    i = i + 1
+
+osf = open('os.txt', 'r').read().split("\n")
+oslogo = [""] * len(osf)
+
+#parse the file that contains the logos
 startRead = False
 i = 0
 for line in osf:
-    
-    if line == osinfo[2].replace("ID=", ''):        
+    if line == osinfo[2].replace("ID=", '').strip():
         startRead = True
         continue;
 
-    if line == ":":
+    if line == ":" and startRead == True:
         break;
 
     if startRead == True:
@@ -29,12 +31,12 @@ for line in osf:
         i = i + 1
 
 
-
+#print everything!
 print()
 print(oslogo[0], "OS:\033[0m", osinfo[0].replace("NAME=", '').replace("\"", ''))
 print(oslogo[1], "Kernel:\033[0m", uname[2])
 print(oslogo[2], "CPU:\033[0m", uname[12] + " " + cpuinfo[4].replace("model name	: ", '').replace(" CPU", ''))
-print(oslogo[3], "User:\033[0m", uname[1])
-print(oslogo[4])
+print(oslogo[3], "User:\033[0m", whoami)
+print(oslogo[4], "Host:\033[0m", uname[1])
 print(oslogo[5])
 print("\033[0m")
