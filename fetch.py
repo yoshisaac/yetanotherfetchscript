f7946c1047e315ffd6337ca6a94f3d20bfeb18d9
yoshisaac <isfortner@gmail.com>
2024-05-13 19:28:31 -0400

    added shell & added os-release parser

--- fetch.py
+++ fetch.py
@@ -6,16 +6,18 @@ import subprocess
 def get_os_info():
     uname = subprocess.check_output("uname -a", shell=True, text=True).split(" ")
     whoami = subprocess.check_output("whoami", shell=True, text=True).strip()
+    arch = subprocess.check_output("uname -m", shell=True, text=True).strip()
+    shell = subprocess.check_output("echo $SHELL", shell=True, text=True).strip()
     cpuinfo = subprocess.check_output("cat /proc/cpuinfo", shell=True, text=True).split("\n")
-    osinfo = subprocess.check_output("cat /etc/os-release", shell=True, text=True).split("\n")
-    return uname, whoami, cpuinfo, osinfo
+    osrelease = subprocess.check_output("cat /etc/os-release", shell=True, text=True).split("\n")
+    return uname, whoami, cpuinfo, osrelease, arch, shell
 
-def parse_os_logo(osinfo, osf):
+def parse_os_logo(ID, osf):
     oslogo = [""] * len(osf)
     startRead = False
     i = 0
     for line in osf:
-        if line == osinfo[2].replace("ID=", '').strip():
+        if line == ID:
             startRead = True
             continue
         if line == ":EOF" and startRead:
@@ -25,25 +27,46 @@ def parse_os_logo(osinfo, osf):
             i += 1
     return oslogo
 
+def parse_os_release(osrelease):
+    ID = ""
+    NAME = ""
+    for element in osrelease:
+        if "ID=" in element and not "BUILD" in element:
+            ID = element.replace("ID=", '').strip()
+        if "NAME=" in element and not "PRETTY" in element:
+            NAME = element.replace("NAME=", '').replace("\"", '').strip()
+    return ID, NAME
+
+def parse_shell(shell):
+    if "bash" in shell:
+        return "bash"
+    elif "zsh" in shell:
+        return "zsh"
+    else:
+        return "Other"
+
 def main():
-    uname, whoami, cpuinfo, osinfo = get_os_info()
+    uname, whoami, cpuinfo, osrelease, arch, shell = get_os_info()
+
+    ID, NAME = parse_os_release(osrelease)
+    shell = parse_shell(shell)
 
     i = 1
     while i < len(sys.argv):
         if sys.argv[i] == "-d" or sys.argv[i] == "--distro":
-            osinfo[2] = sys.argv[i+1]
+            ID = sys.argv[i+1]
         i += 1
 
     osf = open('os.txt', 'r').read().split("\n")
-    oslogo = parse_os_logo(osinfo, osf)
+    oslogo = parse_os_logo(ID, osf)
 
     print()
-    print(oslogo[0], "OS:\033[0m", osinfo[0].replace("NAME=", '').replace("\"", ''))
+    print(oslogo[0], "OS:\033[0m", NAME)
     print(oslogo[1], "Kernel:\033[0m", uname[2])
-    print(oslogo[2], "CPU:\033[0m", uname[12] + " " + cpuinfo[4].replace("model name	: ", '').replace(" CPU", ''))
-    print(oslogo[3], "User:\033[0m", whoami)
+    print(oslogo[2], "CPU:\033[0m", arch + " " + cpuinfo[4].replace("model name	: ", '').replace(" CPU", ''))
+    print(oslogo[3], "Shell:\033[0m", shell)
     print(oslogo[4], "Host:\033[0m", uname[1])
-    print(oslogo[5])
+    print(oslogo[5], "User:\033[0m", whoami)
     print("\033[0m")
 
 if __name__ == "__main__":
