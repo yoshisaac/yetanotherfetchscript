d16fc62c4695cfe15eff312c4edd5d806bb201dc
yoshisaac <isfortner@gmail.com>
2024-05-14 10:36:40 -0400

    changed logo spec

--- fetch.py
+++ fetch.py
@@ -7,25 +7,35 @@ def get_os_info():
     kernel = subprocess.check_output("uname -r", shell=True, text=True).strip()
     whoami = subprocess.check_output("whoami", shell=True, text=True).strip()
     arch = subprocess.check_output("uname -m", shell=True, text=True).strip()
-    host  =subprocess.check_output("cat /etc/hostname", shell=True, text=True).strip()
+    host = subprocess.check_output("cat /etc/hostname", shell=True, text=True).strip()
     shell = subprocess.check_output("echo $SHELL", shell=True, text=True).strip()
+    term = subprocess.check_output("echo $TERM", shell=True, text=True).strip()
     cpuinfo = subprocess.check_output("cat /proc/cpuinfo", shell=True, text=True).split("\n")
     osrelease = subprocess.check_output("cat /etc/os-release", shell=True, text=True).split("\n")
-    return kernel, whoami, cpuinfo, osrelease, arch, shell, host
+    return kernel, whoami, cpuinfo, osrelease, arch, shell, host, term
 
 def parse_os_logo(ID, osf):
     oslogo = [""] * len(osf)
     startRead = False
     i = 0
+    line: str = ""
+    linedex: int = 0
     for line in osf:
+        linedex += 1
         if line == ID:
             startRead = True
+            continue
+        if line[0] == "#":
             continue
         if line == ":EOF" and startRead:
             break
         if startRead:
             oslogo[i] = line.replace("\\033", '\033')
             i += 1
+    while i < len(oslogo) - linedex:
+        if oslogo[i] == "":
+            oslogo[i] = osf[linedex-2].replace("\\033", '\033')
+        i += 1
     return oslogo
 
 def parse_os_release(osrelease):
@@ -46,11 +56,18 @@ def parse_shell(shell):
     else:
         return "Other"
 
+def parse_term(term):
+    if "kitty" in term:
+        return "kitty"
+    else:
+        return term
+    
 def main():
-    kernel, whoami, cpuinfo, osrelease, arch, shell, host = get_os_info()
+    kernel, whoami, cpuinfo, osrelease, arch, shell, host, term = get_os_info()
 
     ID, NAME = parse_os_release(osrelease)
     shell = parse_shell(shell)
+    term = parse_term(term)
 
     i = 1
     while i < len(sys.argv):
@@ -68,6 +85,7 @@ def main():
     print(oslogo[3], "Shell:\033[0m", shell)
     print(oslogo[4], "Host:\033[0m", host)
     print(oslogo[5], "User:\033[0m", whoami)
+    print(oslogo[6], "Term:\033[0m", term)
     print("\033[0m")
 
 if __name__ == "__main__":
