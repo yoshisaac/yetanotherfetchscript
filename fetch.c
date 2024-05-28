a28d56350baa2dd0d3fd3daec1589b1b2138be35
yoshisaac <isfortner@gmail.com>
2024-05-28 19:54:30 -0400

    Add remove unwanted strings

--- fetch.c
+++ fetch.c
@@ -7,10 +7,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-bool match(const char* stra, const char* strb) {
-
-}
-
 const char* get_term(void) {
   const char* terminal = getenv("TERM");
   if (!terminal) return "unknown";
@@ -69,9 +65,10 @@ const char* get_cpu(void) {
   FILE* cpuinfo = fopen("/proc/cpuinfo", "r");
 
   //if your cpu name is more than 256 characters, then you've got your own issues
-  char* cpu_name  = malloc(256); 
-  int line        = 0;
+  char* cpu       = malloc(256); 
   bool past_colon = false;
+  bool add_at     = true;
+  int line        = 0;
   int i           = 0;
   int ch;
   while ((ch = fgetc(cpuinfo)) != EOF) {
@@ -80,20 +77,37 @@ const char* get_cpu(void) {
     if (line == EO_CPU) break;
 
     if (line == MODEL_NAME && past_colon == true) {
-      cpu_name[i] = ch;
+      if (ch == '@') { past_colon = false; continue; }
+      cpu[i] = ch;
+      i++;
+    }
+
+    if (line > MODEL_NAME && add_at == true) {
+      cpu[i] = '@';
+      i++;
+      add_at = false;
+    }
+
+    if (line == CPU_MHZ && past_colon == true) {
+      cpu[i] = ch;
       i++;
     }
   }
-  cpu_name[i] = '\0';
+  cpu[i] = '\0';
 
-  for (unsigned int i = 0; i < strlen(cpu_name); ++i) {
-    if (cpu_name[i] == 'C' && cpu_name[i+1] == 'P' && cpu_name[i+2] == 'U')
-      for (unsigned int h = 0; h < 4; ++h) {
-	cpu_name[i+h] = 6; //acknowledge that we don't want that character
-      }
+  //remove unwanted strings
+  for (unsigned int i = 0; i < strlen(cpu); ++i) {
+    if (cpu[i] == 'C' && cpu[i+1] == 'P' && cpu[i+2] == 'U')
+      for (unsigned int h = 0; h < 4; ++h) cpu[i+h] = 6;
+
+    if (cpu[ i ] == 'P' && cpu[i+1] == 'r' &&
+	cpu[i+2] == 'o' && cpu[i+3] == 'c' &&
+	cpu[i+4] == 'e' && cpu[i+5] == 's' &&
+	cpu[i+6] == 's' && cpu[i+7] == 'o' && cpu[i+8] == 'r')
+      for (unsigned int h = 0; h < 9; ++h) cpu[i+h] = 6;
   }
   
-  return cpu_name;
+  return cpu;
 }
 
 int main(void) {
