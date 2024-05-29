98dceab111c1677f223ae009cb3c54c48b8b0e86
yoshisaac <isfortner@gmail.com>
2024-05-28 23:26:08 -0400

    Add dynamic cpuinfo pars

--- fetch.c
+++ fetch.c
@@ -7,6 +7,18 @@
 #include <string.h>
 #include <stdbool.h>
 
+//for matching a section of a string, not the entire strings
+bool match(const char* stra, const char* strb) {
+  for (unsigned int i = 0; i < strlen(strb); ++i) {
+    if (stra[i] == '\0') return false;
+    
+    if (stra[i] != strb[i])
+      return false;
+  }
+
+  return true;
+}
+
 const char* get_term(void) {
   const char* terminal = getenv("TERM");
   if (!terminal) return "unknown";
@@ -30,70 +42,71 @@ const char* get_shell(void) {
   return shell;
 }
 
-enum CPUINFO {
-  PROCESSOR,
-  VENDOR_ID,
-  CPU_FAMILY,
-  MODEL,
-  MODEL_NAME,
-  STEPPING,
-  MICROCODE,
-  CPU_MHZ,
-  CACHE_SIZE,
-  PHYSICAL_ID,
-  SIBLINGS,
-  CORE_ID,
-  CPU_CORES,
-  APICID,
-  INITIAL_APICID,
-  FPU,
-  FPU_EXCEPTION,
-  CPUID_LEVEL,
-  WP,
-  FLAGS,
-  VMX_FLAGS,
-  BUGS,
-  BOGOMIPS,
-  CLFLUSH_SIZE,
-  CACHE_ALIGNMENT,
-  ADDRESS_SIZES,
-  POWER_MANAGEMENT,
-  EO_CPU
-};
+char* calc_mhz(const char* mhz_str) {
+  double mhz_d;
+  sscanf(mhz_str, "%lf", &mhz_d);
+
+  bool is_ghz = false;
+  if (mhz_d >= 1000) {
+    mhz_d /= 100;
+    is_ghz = true;
+  }
+
+  char* mhz_str2;
+  
+  snprintf(mhz_str2, 256, "%lf", mhz_d);
+
+  return mhz_str2;
+}
 
 const char* get_cpu(void) {
   FILE* cpuinfo = fopen("/proc/cpuinfo", "r");
 
   //if your cpu name is more than 256 characters, then you've got your own issues
-  char* cpu       = malloc(256); 
-  bool past_colon = false;
-  bool add_at     = true;
-  int line        = 0;
-  int i           = 0;
+  char* cpu         = malloc(256);
+  char* cpu_mhz     = malloc(256);
+  char* line        = malloc(256);
+  bool past_colon   = false;
+  bool on_modelname = false;
+  bool on_cpumhz    = false;
+  int cpu_t         = 0;
+  int cpu_mhz_t     = 0;
+  int line_t        = 0;
   int ch;
   while ((ch = fgetc(cpuinfo)) != EOF) {
-    if (ch == '\n')    { past_colon  = false; line++;   } // track which line we're at
-    if (ch == ':')     { past_colon  = true;  continue; } // skip the colon
-    if (line == EO_CPU) break;
+    if (ch == '\n') { past_colon   = false;
+                      on_modelname = false;
+		      on_cpumhz    = false;
+		      line_t       = 0;
+		      memset(line, '\0', 256);
+		      continue; }
 
-    if (line == MODEL_NAME && past_colon == true) {
-      if (ch == '@') { past_colon = false; continue; }
-      cpu[i] = ch;
-      i++;
+    if (ch == ':')  { past_colon   = true;  continue; } // skip the colon
+    line[line_t] = ch;
+    if (match(line, "power management")) break;
+
+    if (match(line, "model name")) on_modelname = true;
+    if (on_modelname && past_colon) {
+      if (ch == '@') { cpu[cpu_t] = '@'; cpu[++cpu_t] = ' '; past_colon = false; continue; }
+      cpu[cpu_t] = ch;
+      ++cpu_t;
     }
 
-    if (line > MODEL_NAME && add_at == true) {
-      cpu[i] = '@';
-      i++;
-      add_at = false;
+    if (match(line, "cpu MHz")) on_cpumhz = true;
+    if (on_cpumhz && past_colon) {
+      if (ch == '.') { past_colon = false; continue; }
+      cpu[cpu_t] = ch;
+      ++cpu_t;
     }
 
-    if (line == CPU_MHZ && past_colon == true) {
-      cpu[i] = ch;
-      i++;
-    }
+    ++line_t;
+    
   }
-  cpu[i] = '\0';
+  cpu[ cpu_t ] = ' ';
+  cpu[cpu_t+1] = 'M';
+  cpu[cpu_t+2] = 'H';
+  cpu[cpu_t+3] = 'z';
+  cpu[cpu_t+4] = '\0';
 
   //remove unwanted strings
   for (unsigned int i = 0; i < strlen(cpu); ++i) {
@@ -106,6 +119,8 @@ const char* get_cpu(void) {
 	cpu[i+6] == 's' && cpu[i+7] == 'o' && cpu[i+8] == 'r')
       for (unsigned int h = 0; h < 9; ++h) cpu[i+h] = 6;
   }
+
+  free(line);
   
   return cpu;
 }
